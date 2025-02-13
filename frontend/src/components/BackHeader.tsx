70946841f347d6b490a1682fb80473ad94a9509b
mjiee <1083294650@qq.com>
2025-02-13 16:26:22 +0800

    feat: complete the internationalization transformation.

--- frontend/src/components/BackHeader.tsx
+++ frontend/src/components/BackHeader.tsx
@@ -1,16 +1,18 @@
 import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
 import { Container, Button, Avatar } from "@mantine/core";
-import styles from "@/styles/header.module.css";
+import styles from "@/assets/styles/header.module.css";
 
-export function HeaderMenu() {
+export function BackHeader() {
   let navigate = useNavigate();
+  const { t } = useTranslation("common");
 
   return (
     <header className={styles.header}>
       <Container size="md" className={styles.inner}>
         <Avatar size={28} name="World News" color="initials" />
         <Button variant="default" onClick={() => navigate(-1)}>
-          Return
+          {t("button.back")}
         </Button>
       </Container>
     </header>
