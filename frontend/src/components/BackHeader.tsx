ac06d1fda3b34b6b738e12c2d93753250b3ad024
mjiee <1083294650@qq.com>
2025-02-27 22:22:16 +0800

    feat: environment variable comments

--- frontend/src/components/BackHeader.tsx
+++ frontend/src/components/BackHeader.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
-import {} from "react-router";
 import { Container, Button, Avatar } from "@mantine/core";
 import styles from "@/assets/styles/header.module.css";
 import appicon from "@/assets/images/appicon.png";
